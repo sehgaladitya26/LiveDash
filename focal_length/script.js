8ef418de4f8c7c04e589acdb44b111ead7ebfc43
sehgaladitya26 <80144935+sehgaladitya26@users.noreply.github.com>
2021-12-20 12:13:34 +0530

    Add files via upload

---
+++ focal_length/script.js
@@ -0,0 +1,115 @@
+var myInterval;
+function myfunction() {
+    console.log("tu pagal hai kya??")
+    document.getElementById("bt1").innerHTML = "Recalibrating";
+    fetch("https://blynk.cloud/external/api/update?token=LvC6vyL_uPSpPdgnlCln0I9Vab6zcogV&v0=" + "1")
+    myInterval = setInterval(myTimer, 500);
+}
+
+function myTimer() {
+    //document.querySelector('form').onsubmit = function () {
+    fetch('https://blynk.cloud/external/api/get?token=LvC6vyL_uPSpPdgnlCln0I9Vab6zcogV&v0')
+        .then(response => response.json())
+        .then(data => {
+            const myJSON = JSON.stringify(data)
+            if (myJSON == "0") {
+                document.getElementById("bt1").innerHTML = "Recalibrate";
+                clearInterval(myInterval);
+                document.querySelector("#u").value = 0;
+                document.querySelector("#v").value = 0;
+                document.querySelector("#us").innerHTML = "0";
+                document.querySelector("#vs").innerHTML = "0";
+            }
+        });
+    // .catch(error => {
+    //     console.log('Error:', error);
+    // })
+    return false;
+}
+
+var saved_u_val;
+var saved_v_val;
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    saved_u_val = sessionStorage.getItem('saved_val_u');
+    saved_v_val = sessionStorage.getItem('saved_val_v');
+    console.log(saved_u_val);
+    console.log(saved_v_val);
+
+    if(saved_u_val == null && saved_v_val == null){
+        saved_u_val = 0;
+        saved_v_val = 0;
+        sessionStorage.setItem('saved_val_u', saved_u_val);
+        sessionStorage.setItem('saved_val_v', saved_v_val);
+        console.log(saved_u_val);
+        console.log(saved_v_val);
+    }
+    console.log('Yes, I am mad');
+    document.querySelector("#vs").innerHTML = saved_v_val;
+    document.querySelector("#us").innerHTML = saved_u_val;
+    document.querySelector("#u").value = saved_u_val;
+    document.querySelector("#v").value = saved_v_val;
+
+    //setInterval(myTimer, 500);
+
+    // document.querySelector('form').onsubmit = function () {
+    //     const target = document.querySelector('#get_bool').value;
+    //     document.querySelector('#result2').innerHTML = target;
+    //     //console.log(target);
+    //     fetch("https://blynk.cloud/external/api/update?token=PIhHUS2rhKB6AlJQeN7TGcMh326zGV4-&v1=" + target)
+    //     // document.querySelector('#result').innerHTML = target;
+    //     return false;
+    // }
+
+    const inputslider_u = document.querySelector("#u");
+    inputslider_u.oninput = () => {
+        let value1 = inputslider_u.value;
+        document.querySelector("#us").innerHTML = value1;
+        fetch("https://blynk.cloud/external/api/update?token=LvC6vyL_uPSpPdgnlCln0I9Vab6zcogV&v1=" + value1)
+        sessionStorage.setItem('saved_val_u', value1);
+        console.log(value1);
+
+        return false;
+    }
+
+    const inputslider_v = document.querySelector("#v");
+    inputslider_v.oninput = () => {
+        let value2 = inputslider_v.value;
+        document.querySelector("#vs").innerHTML = value2;
+        fetch("https://blynk.cloud/external/api/update?token=LvC6vyL_uPSpPdgnlCln0I9Vab6zcogV&v2=" + value2)
+        sessionStorage.setItem('saved_val_v', value2);
+        console.log(value2);
+
+        return false;
+    }
+
+});
+
+function increment(ID) {
+    console.log("Haan main pagal hai for " + ID);
+    const cur = document.getElementById(ID);
+    var v1 = cur.value;
+    v1 = (Number(v1) + Number(cur.step)).toFixed(1);
+    if (Number(v1) > 47) {
+        v1 = 47.0;
+    }
+    cur.value = v1;
+    document.getElementById(ID + "s").innerHTML = v1;
+    console.log(String.fromCharCode(ID.charCodeAt(0) - 68));
+    fetch("https://blynk.cloud/external/api/update?token=LvC6vyL_uPSpPdgnlCln0I9Vab6zcogV&v" + String.fromCharCode(ID.charCodeAt(0) - 68) + "=" + v1)
+}
+
+function decrement(ID) {
+    console.log("Haan main pagal hai for " + ID);
+    const cur = document.getElementById(ID);
+    var v1 = cur.value;
+    v1 = (Number(v1) - Number(cur.step)).toFixed(1);
+    if (Number(v1) < 0.0) {
+        v1 = 0.0;
+    }
+    cur.value = v1;
+    document.getElementById(ID + "s").innerHTML = v1;
+    console.log(String.fromCharCode(ID.charCodeAt(0) - 68));
+    fetch("https://blynk.cloud/external/api/update?token=LvC6vyL_uPSpPdgnlCln0I9Vab6zcogV&v" + String.fromCharCode(ID.charCodeAt(0) - 68) + "=" + v1)
+}
---
+++ sign_in/script_sin.js
@@ -0,0 +1,69 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-analytics.js";
+
+import {
+    getAuth,
+    onAuthStateChanged,
+    createUserWithEmailAndPassword,
+    signOut,
+    signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+
+import {
+    getFirestore,
+    collection,
+    getDocs,
+    addDoc,
+    onSnapshot,
+    query,
+    orderBy,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA_MVJry1gDFJbd_sLw-WZk4cwwF0kDrKA",
+    authDomain: "skarr-3a4a5.firebaseapp.com",
+    projectId: "skarr-3a4a5",
+    storageBucket: "skarr-3a4a5.appspot.com",
+    messagingSenderId: "1040437409550",
+    appId: "1:1040437409550:web:8187021b427c113ab1895f",
+    measurementId: "G-DG6H3K2CTM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Authentication
+const auth = getAuth();
+
+// Initialize firestore
+const db = getFirestore();
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((cred) => {
+            console.log(cred.user)
+
+            //close the login modal and reset the form
+            location.href = "../home_page/home.html";
+            loginForm.reset();
+        })
+        .catch((err)=>{
+            console.log(err.message)
+        })
+});
